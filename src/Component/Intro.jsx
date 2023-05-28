3ea3122b9875b8f6e73df7ae33af7248eb64edb3
Sumat Mallick <115460605+SumatM@users.noreply.github.com>
2023-05-28 11:18:46 +0530

    animation added

--- src/Component/Intro.jsx
+++ src/Component/Intro.jsx
@@ -1,78 +1,126 @@
-import {Box, Text,Button, Heading, Flex, Image}  from '@chakra-ui/react' 
-import imgOlive from './../Banner/IMG20220220130528FAce_edited-removebg-previewOlive.png'
-import resume from './../Banner//Sumat-Mallick-Resume.pdf'
-import {motion} from 'framer-motion'
+import { Box, Text, Button, Heading, Flex, Image } from '@chakra-ui/react'
+import { motion } from 'framer-motion'
 import { useContext } from 'react'
 import { ThemeContext } from './ThemeContext'
-
 import { MdEmail } from 'react-icons/md'
 import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
+import imgOlive from './../Banner/IMG20220220130528FAce_edited-removebg-previewOlive.png'
+import resume from './../Banner/Sumat-Mallick-Resume.pdf'
 
+export default function Intro() {
+  const theme = useContext(ThemeContext)
 
-export default function  Intro(){
+  const container = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        staggerChildren: 0.2,
+        when: "beforeChildren",
+      },
+    },
+  };
 
-    const theme = useContext(ThemeContext)
+  const item = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+  };
 
-    return (
-    <Box id="home" className='about section' background={theme.bg}>
-    
-    <Box  p='15px' display={{base:'none',sm:'none',md:'block'}} position='fixed' left='0' top='35%' >
+  return (
+    <motion.div
+      variants={container}
+      initial="hidden"
+      animate="visible"
+      style={{ position: "relative" }}
+    >
+      <Box
+        id="home"
+        className="about section"
+        background={theme.bg}
+        p="15px"
+        position="relative"
+      >
+        <Box  p='15px' display={{base:'none',sm:'none',md:'block'}} position='fixed' left='0' top='35%' >
+        <motion.div variants={item}>
+          <Flex mt="15px" justify="start">
+            <MdEmail color="#26A69A" size="25px" />
+          </Flex>
+        </motion.div>
 
-            {/* Email */}
-            <Flex  mt='15px' justify='start' >
-            <motion.div whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}  style={{width:'10%'}}>
-            <MdEmail color='#26A69A' size='25px'/>
-            </motion.div>
-            </Flex>
-            {/* linkedin */}
-            <Flex  mt='25px' >
-            <Box w='10%'>
-                <a target="_blank" href="https://www.linkedin.com/in/sumat-mallick-65b966227/">                       
-                <motion.div whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}  style={{width:'10%'}}>
-            <AiFillLinkedin color='#26A69A' size='25px'/>
-            </motion.div></a>
+        <motion.div variants={item}>
+          <Flex mt="25px">
+            <Box w="10%">
+              <a target="_blank" href="https://www.linkedin.com/in/sumat-mallick-65b966227/">
+                <AiFillLinkedin color="#26A69A" size="25px" />
+              </a>
             </Box>
-            </Flex>
-            {/* github */}
-            <Flex  mt='25px' >
-            <Box w='10%'>
-                <a target="_blank"  href="https://github.com/SumatM">                       
-                <motion.div whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}  style={{width:'10%'}}>
-            <AiFillGithub color='#26A69A' size='25px'/>
-            </motion.div></a>
-            </Box>
-            </Flex>
-    </Box>
+          </Flex>
+        </motion.div>
 
-    <Box position='fixed' top={{base:'62%'}} display='inline'>
-        <Box p='5px 15px'  className="nav-link resume" fontWeight='bolder' color='#00796B'>
-           <a href={resume} target='_blank' download="Sumat-Mallick-Resume" id="resume-link-1"  >
-           <Button background='#00796B' color='white' outline="1px solid #26A69A" 
-            _hover={{background:'none',color:'#26A69A'}} borderRadius='0px' mt='10px' id="resume-button-1">Resume</Button>
+        <motion.div variants={item}>
+          <Flex mt="25px">
+            <Box w="10%">
+              <a target="_blank" href="https://github.com/SumatM">
+                <AiFillGithub color="#26A69A" size="25px" />
+              </a>
+            </Box>
+          </Flex>
+        </motion.div>
+        </Box>
+
+        <Box position="fixed" top={{ base: '65%' }} display="inline">
+          <Box className="nav-link resume" fontWeight="bolder" color="#00796B">
+            <a href={resume} target="_blank" download="Sumat-Mallick-Resume" id="resume-link-1">
+              <Button
+                background="#00796B"
+                color="white"
+                outline="1px solid #26A69A"
+                _hover={{ background: 'none', color: '#26A69A' }}
+                borderRadius="0px"
+                mt="10px"
+                id="resume-button-1"
+              >
+                Resume
+              </Button>
             </a>
           </Box>
-    </Box>
+        </Box>
 
-    <Flex pt={{base:'55px',sm:'45px',md:'55px'}} pb='50px'  
-         margin='auto' flexDirection={{base:'column',sm:'column',md:'column'}} w='90%'>
+        <Flex
+          pt={{ base: '55px', sm: '45px', md: '55px' }}
+          pb="50px"
+          margin="auto"
+          flexDirection={{ base: 'column', sm: 'column', md: 'column' }}
+          w="90%"
+        >
+          <Flex w={{ base: '90%', sm: '75%', md: '50%' }} p="10px" margin="auto" flexDirection="column" justify="center" align="center" textAlign="center">
+            <motion.div variants={item}>
+              <Box mb="10px">
+                <Text fontWeight="bold" color="#00796B" fontSize="2xl">
+                  Hello, I am
+                </Text>
+              </Box>
+            </motion.div>
 
-        <Flex w={{base:'90%',sm:'75%',md:'50%'}} p='10px' margin='auto'  flexDirection='column' justify='center' align='center' textAlign='center' >
-            <Box mb='10px'   >
-            <Text fontWeight='bold' color="#00796B" fontSize='2xl'>Hello, I am</Text>
-            <Box mt='15px'>
-            <Text fontWeight='500' fontSize='55px' display='inline' color='#00796B' id="user-detail-name" letterSpacing='2px'>Sumat Mallick</Text>
-            </Box>
-            <Box mt='15px' >
-           <img src="https://readme-typing-svg.demolab.com?weight=400&size=25&pause=1000&color=26A69A&background=FFFFFF00&center=true&vCenter=true&width=435&lines=A+Full+Stack+Web+Developer;A+Problem+Solver" alt="Typing SVG" />
-            </Box>
-            </Box>
-            
+            <motion.div variants={item}>
+              <Box mt="15px">
+                <Text fontWeight="500" fontSize="55px" display="inline" color="#00796B" id="user-detail-name" letterSpacing="2px">
+                  Sumat Mallick
+                </Text>
+              </Box>
+            </motion.div>
+
+            <motion.div variants={item}>
+              <Box mt="15px">
+                <img src="https://readme-typing-svg.demolab.com?weight=400&size=25&pause=1000&color=26A69A&background=FFFFFF00&center=true&vCenter=true&width=435&lines=A+Full+Stack+Web+Developer;A+Problem+Solver" alt="Typing SVG" />
+              </Box>
+            </motion.div>
+          </Flex>
+
+          {/* About Me */}
         </Flex>
-
-        {/* about me */}
-    </Flex>
-    </Box>)
+      </Box>
+    </motion.div>
+  );
 }
--- src/Component/Skills.jsx
+++ src/Component/Skills.jsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Grid, Heading, Text } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import { ThemeContext } from "./ThemeContext";
 import { useContext } from "react";
 
@@ -7,14 +8,23 @@ export default function Skills() {
 
   return (
     <Box bg={theme.bg} p={{ base: "5px", sm: "10px", md: "45px" }} id="skills" pb="50px">
-    <Box w='80%'  margin="auto" borderTop="1.5px solid #00796B"></Box>
-      <Box textAlign="center"   margin="auto"  p={{ base: "25px" }}>
+      <Box w='80%'  margin="auto" borderTop="1.5px solid #00796B"></Box>
+      <Box textAlign="center" margin="auto" p={{ base: "25px" }}>
         <Box p="35px">
           <Heading color="#00796B">SKILLS</Heading>
         </Box>
-        <Grid gap={{ base: "15px", sm: "10px", md: "25px", lg: "25px" }} templateColumns={{ base: "repeat(2,1fr)", sm: "repeat(3,1fr)", md: "repeat(4,1fr)", lg: "repeat(5,1fr)" }}  m='auto' w={{base:'100%',sm:'100%',md:'100%',lg:'77%'}}  >
+        <Grid
+          gap={{ base: "15px", sm: "10px", md: "25px", lg: "25px" }}
+          templateColumns={{ base: "repeat(2,1fr)", sm: "repeat(3,1fr)", md: "repeat(4,1fr)", lg: "repeat(5,1fr)" }}
+          m='auto'
+          w={{base:'100%',sm:'100%',md:'100%',lg:'77%'}}
+        >
           {/* HTML5 */}
-          <Box  bg="white" p="10px" className="skills-card">
+          <motion.div
+            drag
+            whileHover={{ scale: 1.05, boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.3)" }}
+            className="skills-card"
+          >
             <Flex justifyContent="center">
               <img className="skills-card-img" style={{ width: "40%" }} src="https://cdn-icons-png.flaticon.com/512/919/919827.png" />
             </Flex>
@@ -23,9 +33,13 @@ export default function Skills() {
                 HTML5
               </Heading>
             </Box>
-          </Box>
-          {/* css */}
-          <Box bg="white" p="10px" className="skills-card">
+          </motion.div>
+          {/* CSS */}
+          <motion.div
+            drag
+            whileHover={{ scale: 1.05, boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.3)" }}
+            className="skills-card"
+          >
             <Flex justifyContent="center">
               <img className="skills-card-img" style={{ width: "40%" }} src="https://cdn-icons-png.flaticon.com/512/5968/5968242.png" />
             </Flex>
@@ -34,9 +48,13 @@ export default function Skills() {
                 CSS
               </Heading>
             </Box>
-          </Box>
-          {/* js */}
-          <Box bg="white" p="10px" className="skills-card">
+          </motion.div>
+          {/* JavaScript */}
+          <motion.div
+            drag
+            whileHover={{ scale: 1.05, boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.3)" }}
+            className="skills-card"
+          >
             <Flex justifyContent="center">
               <img className="skills-card-img" style={{ width: "40%" }} src="https://cdn-icons-png.flaticon.com/512/5968/5968292.png" />
             </Flex>
@@ -45,9 +63,13 @@ export default function Skills() {
                 JavaScript
               </Heading>
             </Box>
-          </Box>
-          {/* chakra Ui */}
-          <Box bg="white" p="10px" className="skills-card">
+          </motion.div>
+          {/* Chakra UI */}
+          <motion.div
+            drag
+            whileHover={{ scale: 1.05, boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.3)" }}
+            className="skills-card"
+          >
             <Flex justifyContent="center">
               <img className="skills-card-img" style={{ width: "40%" }} src="https://img.icons8.com/color/512/chakra-ui.png" />
             </Flex>
@@ -56,9 +78,13 @@ export default function Skills() {
                 Chakra UI
               </Heading>
             </Box>
-          </Box>
-          {/* redux */}
-          <Box bg="white" p="10px" className="skills-card">
+          </motion.div>
+          {/* Redux */}
+          <motion.div
+            drag
+            whileHover={{ scale: 1.05, boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.3)" }}
+            className="skills-card"
+          >
             <Flex justifyContent="center">
               <img className="skills-card-img" style={{ width: "40%" }} src="https://img.uxwing.com/wp-content/themes/uxwing/download/brands-social-media/redux-icon.png" />
             </Flex>
@@ -67,9 +93,13 @@ export default function Skills() {
                 Redux
               </Heading>
             </Box>
-          </Box>
-          {/* git */}
-          <Box bg="white" p="10px" className="skills-card">
+          </motion.div>
+          {/* Git */}
+          <motion.div
+            drag
+            whileHover={{ scale: 1.05, boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.3)" }}
+            className="skills-card"
+          >
             <Flex justifyContent="center">
               <img className="skills-card-img" style={{ width: "40%" }} src="https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png" />
             </Flex>
@@ -78,9 +108,13 @@ export default function Skills() {
                 Git
               </Heading>
             </Box>
-          </Box>
-          {/* react */}
-          <Box bg="white" p="10px" className="skills-card">
+          </motion.div>
+          {/* React */}
+          <motion.div
+            drag
+            whileHover={{ scale: 1.05, boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.3)" }}
+            className="skills-card"
+          >
             <Flex justifyContent="center">
               <img className="skills-card-img" style={{ width: "40%" }} src="https://cdn.iconscout.com/icon/free/png-256/react-2752089-2284906.png" />
             </Flex>
@@ -89,9 +123,13 @@ export default function Skills() {
                 React
               </Heading>
             </Box>
-          </Box>
-          {/* typescript */}
-          <Box bg="white" p="10px" className="skills-card">
+          </motion.div>
+          {/* TypeScript */}
+          <motion.div
+            drag
+            whileHover={{ scale: 1.05, boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.3)" }}
+            className="skills-card"
+          >
             <Flex justifyContent="center">
               <img className="skills-card-img" style={{ width: "40%" }} src="https://w7.pngwing.com/pngs/915/519/png-transparent-typescript-hd-logo-thumbnail.png" />
             </Flex>
@@ -100,9 +138,13 @@ export default function Skills() {
                 TypeScript
               </Heading>
             </Box>
-          </Box>
-          {/* express */}
-          <Box bg="white" p="10px" className="skills-card">
+          </motion.div>
+          {/* Express */}
+          <motion.div
+            drag
+            whileHover={{ scale: 1.05, boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.3)" }}
+            className="skills-card"
+          >
             <Flex justifyContent="center">
               <img className="skills-card-img" style={{ width: "40%" }} src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT-Z4-b5_nW5k3bO7rgZ09JvnHZTA0I_FHLu4OscCKDv4MDEp6B" />
             </Flex>
@@ -111,9 +153,13 @@ export default function Skills() {
                 Express
               </Heading>
             </Box>
-          </Box>
-          {/* mongodb */}
-          <Box bg="white" p="10px" className="skills-card">
+          </motion.div>
+          {/* MongoDB */}
+          <motion.div
+            drag
+            whileHover={{ scale: 1.05, boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.3)" }}
+            className="skills-card"
+          >
             <Flex justifyContent="center">
               <img className="skills-card-img" style={{ width: "40%" }} src="https://devkico.itexto.com.br/wp-content/uploads/2013/10/mongodb-leaf-300x300.png" />
             </Flex>
@@ -122,9 +168,13 @@ export default function Skills() {
                 MongoDB
               </Heading>
             </Box>
-          </Box>
-          {/* node */}
-          <Box bg="white" p="10px" className="skills-card">
+          </motion.div>
+          {/* Node.js */}
+          <motion.div
+            drag
+            whileHover={{ scale: 1.05, boxShadow: "0px 5px 10px rgba(0, 0, 0, 0.3)" }}
+            className="skills-card"
+          >
             <Flex justifyContent="center">
               <img className="skills-card-img" style={{ width: "40%" }} src="https://pluralsight2.imgix.net/paths/images/nodejs-45adbe594d.png" />
             </Flex>
@@ -133,7 +183,7 @@ export default function Skills() {
                 Node.js
               </Heading>
             </Box>
-          </Box>
+          </motion.div>
         </Grid>
       </Box>
     </Box>
